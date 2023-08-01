0699428d1cb66038eba2f6558562f173ab86f4e0
Meta Blvk <metablvk@gmail.com>
2023-07-31 20:52:34 -0600

    chore: added protection on routes

--- src/routes/user/user.route.ts
+++ src/routes/user/user.route.ts
@@ -8,10 +8,14 @@ import {
   getUserProfile,
   updateUserProfile,
 } from '../../controllers/user/user.controller';
+import {protect} from '../../middleware/auth.middleware';
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
